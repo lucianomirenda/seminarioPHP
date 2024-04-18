ab79d943cb987c4b3f403a3e9971a80a4d0450e0
lucianodev <mirendaluciano@gmail.com>
2024-04-18 16:25:17 -0300

    solucion en el post de inquilinos y propieades

--- slim/index.php
+++ slim/index.php
@@ -652,10 +652,14 @@ $app->post('/inquilinos',function(Request $request,Response $response){
     $requiredKeys = ["nombre","apellido","documento","email","activo"];
     $missingKeys = []; //almaceno las claves que faltan
 
-    foreach($params as $key => $value){
-        if(in_array($key,$requiredKeys)){
+
+    foreach($requiredKeys as $key){
+        if(!array_key_exists($key, $params)){
+            $missingKeys[] = $key;
+        } else {
+            $value = $params[$key];
             if(empty($value)){
-                $missingKeys[] = $key; //las agrego al array
+                $missingKeys[] = $key; 
             }
         }
     }
@@ -809,5 +813,94 @@ $app->get('/inquilinos/{id}',function (Request $request, Response $response){
     }
 });
 
+$app -> post ('/propiedades', function (Request $request, Response $response){
+
+    $connection = getConnection();
+
+    $params = $request->getParsedBody();
+    
+    $requiredKeys = ["domicilio","localidad_id","cantidad_huespedes","fecha_inicio_disponibilidad","cantidad_dias","disponible","valor_noche","tipo_propiedad_id"];
+    $missingKeys = [];
+
+
+    foreach($requiredKeys as $key){
+        if(!array_key_exists($key, $params)){
+            $missingKeys[] = $key;
+        } else {
+            $value = $params[$key];
+            if(empty($value)){
+                $missingKeys[] = $key; 
+            }
+        }
+    }
+    
+    if(empty($missingKeys)){
+
+        $stmt = $connection->prepare("SELECT * FROM localidades WHERE id = :localidad_id");
+        $stmt->bindParam(':localidad_id',$params['id']);
+        $stmt->execute();
+
+        if (!$stmt->rowCount() > 0) {
+
+            $stmt = $connection->prepare("SELECT * FROM tipo_propiedades WHERE id = :tipo_propiedad_id");
+            $stmt->bindParam(':tipo_propiedad_id',$params['id']);
+            $stmt->execute();
+
+            if (!$stmt->rowCount() > 0) {
+            
+                $stmt = $connection->prepare("INSERT INTO propiedades(domicilio,localidad_id,cantidad_habitaciones,cantidad_banios,cochera,cantidad_huespedes,fecha_inicio_disponibilidad,cantidad_dias,disponible,valor_noche,tipo_propiedad_id,imagen,tipo_imagen)
+                                            VALUES (:domicilio, :localidad_id, :cantidad_habitaciones, :cantidad_banios, :cochera, :cantidad_huespedes, :fecha_inicio_disponibilidad, :cantidad_dias, :disponible, :valor_noche, :tipo_propiedad_id, :imagen, :tipo_imagen)");
+
+                $stmt->bindParam(':domicilio',$params['domicilio']);
+                $stmt->bindParam(':localidad_id',$params['localidad_id']);
+                $stmt->bindParam(':cantidad_habitaciones',$params['cantidad_habitaciones']);
+                $stmt->bindParam(':cantidad_banios',$params['cantidad_banios']);
+                $stmt->bindParam(':cochera',$params['cochera']);
+                $stmt->bindParam(':cantidad_huespedes',$params['cantidad_huespedes']);
+                $stmt->bindParam(':fecha_inicio_disponibilidad',$params['fecha_inicio_disponibilidad']);
+                $stmt->bindParam(':cantidad_dias',$params['cantidad_dias']);
+                $stmt->bindParam(':disponible',$params['disponible']);
+                $stmt->bindParam(':valor_noche',$params['valor_noche']);
+                $stmt->bindParam(':tipo_propiedad_id',$params['tipo_propiedad_id']);
+                $stmt->bindParam(':imagen',$params['imagen']);
+                $stmt->bindParam(':tipo_imagen',$params['tipo_imagen']);
+                $stmt->execute();
+
+                $payload = json_encode([
+                    'message' => 'La propiedad se inserto en la base de datos correctamente.',
+                    'status' => 'success',
+                    'code' => 201,
+                    'data' => $params
+                ]);
+            
+            } else {
+                $payload = json_encode([
+                    'message' => 'El tipo de propiedad no existe.',
+                    'status' => 'Error',
+                    'code' => 400,
+                ]);
+            }
+        } else {
+            $payload = json_encode([
+                'message' => 'La localidad no existe.',
+                'status' => 'Error',
+                'code' => 400,
+            ]);
+        }
+                
+    } else {
+        $payload = json_encode([
+            'message' => 'Falta completar los siguientes campos',
+            'status' => 'Error',
+            'code' => 400,
+            'data' => $missingKeys
+        ]);
+    } 
+    
+
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type','application/json');
+
+});
 
 $app->run();
